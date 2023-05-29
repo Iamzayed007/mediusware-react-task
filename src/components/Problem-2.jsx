040b7feebe5cc2f129ee4998a586905bc60d64a4
Md. Zayed Hasan <72243831+Iamzayed007@users.noreply.github.com>
2023-05-29 22:32:34 +0600

    update

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { createBrowserHistory } from 'history';
 const Problem2 = () => {
-
+ const [modalType,setModalType] =useState('')
+ const [countryList,setCountryList] =useState([])
+ const history = createBrowserHistory();
+ const handleAllCountry=(type)=>{
+ setModalType(type)
+//  history.push('/problem-2/country')
+ console.log(modalType);
+ }
+ useEffect(()=>{
+    // if (modalType === 'allContacts') {
+        fetch('https://contact.mediusware.com/api/contacts/',{
+            headers:{
+                "Access-Control-Allow-Origin": "http://localhost:5173/"
+            }
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log("all",data);
+            setCountryList(data)
+            console.log(countryList);
+        })
+    // }
+ },[])
     return (
 
         <div className="container">
@@ -9,8 +31,8 @@ const Problem2 = () => {
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
                 
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                <button className="btn btn-lg btn-outline-primary" type="button" onClick={()=>handleAllCountry('allContacts')} >All Contacts</button>
+                <button className="btn btn-lg btn-outline-warning" type="button" onClick={()=>handleAllCountry('usContacts')} >US Contacts</button>
                 </div>
                 
             </div>
